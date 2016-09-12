4e6b6c79d335f191f46cbb9e383477ab087f4b3d
chintalniit <sanghvichintal19@gmail.com>
2016-09-12 15:40:25 +0530

    Add files via upload

---
+++ ShoesKart/src/main/java/com/shoes/controller/ShoesController.java
@@ -0,0 +1,44 @@
+package com.shoes.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.shoes.dao.RegisterDao;
+import com.shoes.model.Register;
+
+@Controller
+public class ShoesController {
+@Autowired
+RegisterDao registerdao;
+	@RequestMapping("/")
+	public String index()
+{
+		System.out.println("index called");
+ return "index";
+}
+	@RequestMapping("/login")
+public String login()
+{
+		System.out.println("login called");
+ return "login";
+}
+	@RequestMapping("/register")
+	public ModelAndView register()
+	{
+			System.out.println("register called");
+			Register reg=new Register();
+	 return new ModelAndView("register","reg",reg);
+	}
+	
+	@RequestMapping("/registerUser")
+	public String registerUser(@ModelAttribute("reg")Register register)
+	{
+		System.out.println("registerUser called");
+		registerdao.registerUser(register);
+			
+	 return "registerUser";
+	}
+}
---
+++ ShoesKart/src/main/java/com/shoes/dao/RegisterDao.java
@@ -0,0 +1,9 @@
+package com.shoes.dao;
+
+import com.shoes.model.Register;
+
+public interface RegisterDao  {
+	void registerUser(Register register);
+
+
+}
